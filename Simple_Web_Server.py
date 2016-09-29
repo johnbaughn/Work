bbd4ceb1b20aad94ff7a5e85bf930b497e84fefb
John B <john.baughn@gmail.com>
2016-09-28 18:32:48 -0700

    Basic HTTP Server

--- Simple_Web_Server.py
+++ Simple_Web_Server.py
@@ -5,7 +5,7 @@ PORT = 9099
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-httpd = socketserver.TCPServer(("/home/", PORT), Handler)
+httpd = socketserver.TCPServer(("", PORT), Handler)
 
 print("Serving at port", PORT)
 httpd.serve_forever()
